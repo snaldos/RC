0077b8ac47e1ab048eedd6ce547ee4e879295021
snaldos <arnaldoflopes@gmail.com>
2025-10-02 14:15:16 +0100

    git init

---
+++ LAB1/write_noncanonical.c
@@ -0,0 +1,267 @@
+// Example of how to write to the serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+#define BAUDRATE 38400
+#define BUF_SIZE 256
+
+#define FLAG 0x7e
+#define A_SENDER 0x03
+#define A_RECEIVER 0x01
+#define C_SET 0x03
+#define C_UA 0x07
+
+enum SUPERVISION_STATE { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, SUCCESS };
+enum SUPERVISION_STATE supervision_state = START;
+
+int fd = -1;           // File descriptor for open serial port
+struct termios oldtio; // Serial port settings to restore on closing
+volatile int STOP = FALSE;
+
+int openSerialPort(const char *serialPort, int baudRate);
+int closeSerialPort();
+int readByteSerialPort(unsigned char *byte);
+int writeBytesSerialPort(const unsigned char *bytes, int nBytes);
+
+// ---------------------------------------------------
+// MAIN
+// ---------------------------------------------------
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Incorrect program usage\n"
+           "Usage: %s <SerialPort>\n"
+           "Example: %s /dev/ttyS0\n",
+           argv[0], argv[0]);
+    exit(1);
+  }
+
+  // Open serial port device for reading and writing, and not as controlling tty
+  // because we don't want to get killed if linenoise sends CTRL-C.
+  //
+  // NOTE: See the implementation of the serial port library in "serial_port/".
+  const char *serialPort = argv[1];
+
+  if (openSerialPort(serialPort, BAUDRATE) < 0) {
+    perror("openSerialPort");
+    exit(-1);
+  }
+
+  printf("Serial port %s opened\n", serialPort);
+
+  // Create string to send
+  unsigned char buf[BUF_SIZE] = {0};
+
+  buf[0] = FLAG;
+  buf[1] = A_SENDER;
+  buf[2] = C_SET;
+  buf[3] = A_SENDER ^ C_SET;
+  buf[4] = FLAG;
+
+  // In non-canonical mode, '\n' does not end the writing.
+  // Test this condition by placing a '\n' in the middle of the buffer.
+  // The whole buffer must be sent even with the '\n'.
+
+  int bytes = writeBytesSerialPort(buf, BUF_SIZE);
+  printf("%d bytes written to serial port\n", bytes);
+
+  // Wait until all bytes have been written to the serial port
+  sleep(1);
+
+  int nBytesBuf = 0;
+
+  while (STOP == FALSE) {
+    // NOTE: This while() cycle is a simple example showing how to read from the
+    // serial port. It must be changed in order to respect the specifications of
+    // the protocol indicated in the Lab guide.
+
+    // Read one byte from serial port
+    // NOTE: This function may return even if no byte was received, which may
+    // not be true.
+    unsigned char byte;
+    int bytes = readByteSerialPort(&byte);
+
+    if (bytes < 0) {
+      perror("readByteSerialPort");
+      exit(-1);
+    } else if (bytes == 0) {
+      continue; // No byte received, try again
+    }
+
+    buf[nBytesBuf] = byte;
+    nBytesBuf += bytes;
+
+    if (supervision_state == START) {
+      if (byte == FLAG) {
+        printf("Tracking UA FRAME...\n");
+        supervision_state = FLAG_RCV;
+      } else {
+        supervision_state = START;
+        STOP = TRUE;
+      }
+    } else if (supervision_state == FLAG_RCV) {
+      if (byte == A_RECEIVER) {
+        supervision_state = A_RCV;
+      } else {
+        supervision_state = START;
+        STOP = TRUE;
+      }
+    } else if (supervision_state == A_RCV) {
+      if (byte == C_UA) {
+        supervision_state = C_RCV;
+      } else {
+        supervision_state = START;
+        STOP = TRUE;
+      }
+    } else if (supervision_state == C_RCV) {
+      if (byte == (buf[1] ^ buf[2])) {
+        supervision_state = BCC_OK;
+      } else {
+        supervision_state = START;
+        STOP = TRUE;
+      }
+    } else if (supervision_state == BCC_OK) {
+      if (byte == FLAG) {
+        supervision_state = SUCCESS;
+
+      } else {
+        supervision_state = START;
+        STOP = TRUE;
+      }
+    }
+    if (supervision_state == SUCCESS) {
+      printf("UA frame received successfully\n");
+      STOP = TRUE;
+    }
+  }
+
+  // Close serial port
+  if (closeSerialPort() < 0) {
+    perror("closeSerialPort");
+    exit(-1);
+  }
+
+  printf("Serial port %s closed\n", serialPort);
+
+  return 0;
+}
+
+// ---------------------------------------------------
+// SERIAL PORT LIBRARY IMPLEMENTATION
+// ---------------------------------------------------
+
+// Open and configure the serial port.
+// Returns -1 on error.
+int openSerialPort(const char *serialPort, int baudRate) {
+  // Open with O_NONBLOCK to avoid hanging when CLOCAL
+  // is not yet set on the serial port (changed later)
+  int oflags = O_RDWR | O_NOCTTY | O_NONBLOCK;
+  fd = open(serialPort, oflags);
+  if (fd < 0) {
+    perror(serialPort);
+    return -1;
+  }
+
+  // Save current port settings
+  if (tcgetattr(fd, &oldtio) == -1) {
+    perror("tcgetattr");
+    return -1;
+  }
+
+  // Convert baud rate to appropriate flag
+
+  // Baudrate settings are defined in <asm/termbits.h>, which is included by
+  // <termios.h>
+#define CASE_BAUDRATE(baudrate)                                                \
+  case baudrate:                                                               \
+    br = B##baudrate;                                                          \
+    break;
+
+  tcflag_t br;
+  switch (baudRate) {
+    CASE_BAUDRATE(1200);
+    CASE_BAUDRATE(1800);
+    CASE_BAUDRATE(2400);
+    CASE_BAUDRATE(4800);
+    CASE_BAUDRATE(9600);
+    CASE_BAUDRATE(19200);
+    CASE_BAUDRATE(38400);
+    CASE_BAUDRATE(57600);
+    CASE_BAUDRATE(115200);
+  default:
+    fprintf(stderr, "Unsupported baud rate (must be one of 1200, 1800, 2400, "
+                    "4800, 9600, 19200, 38400, 57600, 115200)\n");
+    return -1;
+  }
+#undef CASE_BAUDRATE
+
+  // New port settings
+  struct termios newtio;
+  memset(&newtio, 0, sizeof(newtio));
+
+  newtio.c_cflag = br | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
+
+  // Set input mode (non-canonical, no echo,...)
+  newtio.c_lflag = 0;
+  newtio.c_cc[VTIME] = 0; // Block reading
+  newtio.c_cc[VMIN] = 1;  // Byte by byte
+
+  tcflush(fd, TCIOFLUSH);
+
+  // Set new port settings
+  if (tcsetattr(fd, TCSANOW, &newtio) == -1) {
+    perror("tcsetattr");
+    close(fd);
+    return -1;
+  }
+
+  // Clear O_NONBLOCK flag to ensure blocking reads
+  oflags ^= O_NONBLOCK;
+  if (fcntl(fd, F_SETFL, oflags) == -1) {
+    perror("fcntl");
+    close(fd);
+    return -1;
+  }
+
+  return fd;
+}
+
+// Restore original port settings and close the serial port.
+// Returns 0 on success and -1 on error.
+int closeSerialPort() {
+  // Restore the old port settings
+  if (tcsetattr(fd, TCSANOW, &oldtio) == -1) {
+    perror("tcsetattr");
+    return -1;
+  }
+
+  return close(fd);
+}
+
+// Wait up to 0.1 second (VTIME) for a byte received from the serial port.
+// Must check whether a byte was actually received from the return value.
+// Save the received byte in the "byte" pointer.
+// Returns -1 on error, 0 if no byte was received, 1 if a byte was received.
+int readByteSerialPort(unsigned char *byte) { return read(fd, byte, 1); }
+
+// Write up to numBytes from the "bytes" array to the serial port.
+// Must check how many were actually written in the return value.
+// Returns -1 on error, otherwise the number of bytes written.
+int writeBytesSerialPort(const unsigned char *bytes, int nBytes) {
+  return write(fd, bytes, nBytes);
+}
