ff00e9c2ffd68d5dd427184edc0d57bde0fc4f9d
snaldos <arnaldoflopes@gmail.com>
2025-10-13 14:12:42 +0100

    separation of concerns from app layer to ll

--- lab1-code-25-26/src/application_layer.c
+++ lab1-code-25-26/src/application_layer.c
@@ -7,77 +7,69 @@
 #include <string.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
-                      int nTries, int timeout, const char *filename)
-{
-    LinkLayer ll;
-    strncpy(ll.serialPort, serialPort, sizeof(ll.serialPort) - 1);
-    ll.serialPort[sizeof(ll.serialPort) - 1] = '\0';
-    ll.baudRate = baudRate;
-    ll.nRetransmissions = nTries;
-    ll.timeout = timeout;
+                      int nTries, int timeout, const char *filename) {
+  LinkLayer ll;
+  strncpy(ll.serialPort, serialPort, sizeof(ll.serialPort) - 1);
+  ll.serialPort[sizeof(ll.serialPort) - 1] = '\0';
+  ll.baudRate = baudRate;
+  ll.nRetransmissions = nTries;
+  ll.timeout = timeout;
 
-    if (strcmp(role, "tx") == 0)
-        ll.role = LlTx;
-    else if (strcmp(role, "rx") == 0)
-        ll.role = LlRx;
-    else {
-        fprintf(stderr, "Failed to set link layer role\n");
-        return;
-    }
+  if (strcmp(role, "tx") == 0)
+    ll.role = LlTx;
+  else if (strcmp(role, "rx") == 0)
+    ll.role = LlRx;
+  else {
+    fprintf(stderr, "Failed to set link layer role\n");
+    return;
+  }
 
-    if (llopen(ll) < 0) {
-        fprintf(stderr, "Failed to open link layer\n");
-        return;
-    }
+  if (llopen(ll) < 0) {
+    fprintf(stderr, "Failed to open link layer\n");
+    return;
+  }
 
-    if (ll.role == LlTx) {
-        FILE *file = fopen(filename, "rb");
-        if (file == NULL) {
-            fprintf(stderr, "Failed to open file %s\n", filename);
-            llclose();
-            return;
-        }
-
-        // Buffer para armazenar chunks de 1000 bytes
-        unsigned char buffer[MAX_PAYLOAD_SIZE]; // MAX_PAYLOAD_SIZE = 1000
-        int bytesRead;
-
-        printf("Starting file transmission: %s\n", filename);
-
-        // Ler arquivo em chunks de 1000 bytes
-        while ((bytesRead = fread(buffer, 1, MAX_PAYLOAD_SIZE, file)) > 0) {
-            printf("Read %d bytes from file\n", bytesRead);
-
-            // Enviar chunk via link layer
-            int bytesWritten = llwrite(buffer, bytesRead);
-
-            while (bytesWritten != bytesRead) {
-                if (bytesWritten < 0) {
-                    fprintf(stderr, "Failed to send data chunk\n");
-                    return;
-                } else if (bytesWritten < bytesRead) {
-                    ll.nRetransmissions--;
-                     if (ll.nRetransmissions < 0) {
-                        fprintf(stderr, "Maximum retransmissions reached\n");
-                        fclose(file);
-                        llclose();
-                        return;
-                    }
-                }
-                bytesWritten = llwrite(buffer, bytesRead);
-            }
-
-            printf("Sent %d bytes\n", bytesRead);
-        }
-
-        fclose(file);
-        printf("File transmission completed\n");
-
-    } else if (ll.role == LlRx) {
-    }
-
-    if (llclose() < 0) {
-      fprintf(stderr, "Failed to close link layer\n");
+  if (ll.role == LlTx) {
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+      fprintf(stderr, "Failed to open file %s\n", filename);
+      llclose();
       return;
     }
+
+    // Buffer to store chunks of 1000 bytes
+    unsigned char buffer[MAX_PAYLOAD_SIZE]; // MAX_PAYLOAD_SIZE = 1000
+    int bytesRead;
+
+    printf("Starting file transmission: %s\n", filename);
+
+    // Read file in chunks of 1000 bytes
+    while ((bytesRead = fread(buffer, 1, MAX_PAYLOAD_SIZE, file)) > 0) {
+      printf("Read %d bytes from file\n", bytesRead);
+
+      // Send chunk via link layer
+      int bytesWritten = llwrite(buffer, bytesRead);
+
+      // TODO: do not do retransmissions in application layer do it in ll
+
+      if (bytesWritten != bytesRead) {
+        // TODO: think of the print better
+        fprintf(stderr, "Maximum retransmissions reached\n");
+        fclose(file);
+        llclose();
+        return;
+      }
+
+      printf("Sent %d bytes\n", bytesRead);
+    }
+
+    fclose(file);
+    printf("File transmission completed\n");
+  } else if (ll.role == LlRx) {
+  }
+
+  if (llclose() < 0) {
+    fprintf(stderr, "Failed to close link layer\n");
+    return;
+  }
 }
--- lab1-code-25-26/src/link_layer.c
+++ lab1-code-25-26/src/link_layer.c
@@ -11,220 +11,263 @@
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
+// TODO: may have to change exits to return -1
+
+static LinkLayer ll_config;
+static int ll_opened = 0; // flag to check if llopen() was called
+
+static int transmit_frame(const unsigned char *buf, int bufSize) {
+  int bytesWritten = writeBytesSerialPort(buf, bufSize);
+  return bytesWritten;
+}
+
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
-int llopen(LinkLayer connectionParameters)
-{
+int llopen(LinkLayer connectionParameters) {
 
-    if (openSerialPort(connectionParameters.serialPort, connectionParameters.baudRate) < 0) {
-        perror("openSerialPort");
-        exit(-1);
+  if (openSerialPort(connectionParameters.serialPort,
+                     connectionParameters.baudRate) < 0) {
+    perror("openSerialPort");
+    return -1;
+  }
+
+  printf("Serial port %s opened\n", connectionParameters.serialPort);
+  ll_config = connectionParameters;
+  ll_opened = 1;
+
+  volatile int STOP = FALSE;
+  enum SUPERVISION_STATE set_frame_state = START;
+
+  if (connectionParameters.role == LlTx) {
+    unsigned char buf[BUF_SIZE] = {0};
+
+    buf[0] = FLAG;
+    buf[1] = A_SENDER;
+    buf[2] = C_SET;
+    buf[3] = A_SENDER ^ C_SET;
+    buf[4] = FLAG;
+
+    // In non-canonical mode, '\n' does not end the writing.
+    // Test this condition by placing a '\n' in the middle of the buffer.
+    // The whole buffer must be sent even with the '\n'.
+
+    int bytes = writeBytesSerialPort(buf, BUF_SIZE);
+    printf("%d bytes written to serial port\n", bytes);
+
+    // Wait until all bytes have been written to the serial port
+    sleep(1);
+
+    int nBytesBuf = 0;
+
+    while (STOP == FALSE) {
+      // NOTE: This while() cycle is a simple example showing how to read from
+      // the serial port. It must be changed in order to respect the
+      // specifications of the protocol indicated in the Lab guide.
+
+      // Read one byte from serial port
+      // NOTE: This function may return even if no byte was received, which may
+      // not be true.
+      unsigned char byte;
+      int bytes = readByteSerialPort(&byte);
+
+      if (bytes < 0) {
+        perror("readByteSerialPort");
+        return -1;
+      } else if (bytes == 0) {
+        continue; // No byte received, try again
+      }
+
+      buf[nBytesBuf] = byte;
+      nBytesBuf += bytes;
+
+      if (set_frame_state == START) {
+        if (byte == FLAG) {
+          printf("Tracking UA FRAME...\n");
+          set_frame_state = FLAG_RCV;
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      } else if (set_frame_state == FLAG_RCV) {
+        if (byte == A_RECEIVER) {
+          set_frame_state = A_RCV;
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      } else if (set_frame_state == A_RCV) {
+        if (byte == C_UA) {
+          set_frame_state = C_RCV;
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      } else if (set_frame_state == C_RCV) {
+        if (byte == (buf[1] ^ buf[2])) {
+          set_frame_state = BCC_OK;
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      } else if (set_frame_state == BCC_OK) {
+        if (byte == FLAG) {
+          set_frame_state = SUCCESS;
+
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      }
+      if (set_frame_state == SUCCESS) {
+        printf("UA frame received successfully\n");
+        STOP = TRUE;
+      }
     }
+  } else if (connectionParameters.role == LlRx) {
+    unsigned char buf[BUF_SIZE] = {0};
 
-    printf("Serial port %s opened\n", connectionParameters.serialPort);
+    int nBytesBuf = 0;
 
-    volatile int STOP = FALSE;
-    enum SUPERVISION_STATE set_frame_state = START;
+    while (STOP == FALSE) {
+      // Read one byte from serial port.
+      // NOTE: You must check how many bytes were actually read by reading the
+      // return value. In this example, we assume that the byte is always read,
+      // which may not be true.
+      unsigned char byte;
+      int bytes = readByteSerialPort(&byte);
 
-    if (connectionParameters.role == LlTx) {
-        unsigned char buf[BUF_SIZE] = {0};
+      if (bytes < 0) {
+        perror("readByteSerialPort");
+        return -1;
+      } else if (bytes == 0) {
+        continue; // No byte received, try again
+      }
 
-        buf[0] = FLAG;
-        buf[1] = A_SENDER;
-        buf[2] = C_SET;
-        buf[3] = A_SENDER ^ C_SET;
-        buf[4] = FLAG;
+      buf[nBytesBuf] = byte;
+      nBytesBuf += bytes;
 
-        // In non-canonical mode, '\n' does not end the writing.
-        // Test this condition by placing a '\n' in the middle of the buffer.
-        // The whole buffer must be sent even with the '\n'.
+      if (set_frame_state == START) {
+        if (byte == FLAG) {
+          set_frame_state = FLAG_RCV;
+        }
+      } else if (set_frame_state == FLAG_RCV) {
+        if (byte == A_SENDER) {
+          set_frame_state = A_RCV;
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      } else if (set_frame_state == A_RCV) {
+        if (byte == C_SET) {
+          set_frame_state = C_RCV;
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      } else if (set_frame_state == C_RCV) {
+        if (byte == (buf[1] ^ buf[2])) {
+          set_frame_state = BCC_OK;
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      } else if (set_frame_state == BCC_OK) {
+        if (byte == FLAG) {
+          set_frame_state = SUCCESS;
 
-        int bytes = writeBytesSerialPort(buf, BUF_SIZE);
-        printf("%d bytes written to serial port\n", bytes);
+        } else {
+          set_frame_state = START;
+          STOP = TRUE;
+        }
+      }
+      if (set_frame_state == SUCCESS) {
+        printf("SET frame received successfully\n");
+        unsigned char ua_frame[5] = {FLAG, A_RECEIVER, C_UA, A_RECEIVER ^ C_UA,
+                                     FLAG};
+
+        bytes = writeBytesSerialPort(ua_frame, 5);
+        printf("UA frame sent\n");
 
         // Wait until all bytes have been written to the serial port
         sleep(1);
-
-        int nBytesBuf = 0;
-
-        while (STOP == FALSE) {
-          // NOTE: This while() cycle is a simple example showing how to read from the
-          // serial port. It must be changed in order to respect the specifications of
-          // the protocol indicated in the Lab guide.
-        
-          // Read one byte from serial port
-          // NOTE: This function may return even if no byte was received, which may
-          // not be true.
-          unsigned char byte;
-          int bytes = readByteSerialPort(&byte);
-        
-          if (bytes < 0) {
-            perror("readByteSerialPort");
-            exit(-1);
-          } else if (bytes == 0) {
-            continue; // No byte received, try again
-          }
-      
-          buf[nBytesBuf] = byte;
-          nBytesBuf += bytes;
-      
-          if (set_frame_state == START) {
-            if (byte == FLAG) {
-              printf("Tracking UA FRAME...\n");
-              set_frame_state = FLAG_RCV;
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          } else if (set_frame_state == FLAG_RCV) {
-            if (byte == A_RECEIVER) {
-              set_frame_state = A_RCV;
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          } else if (set_frame_state == A_RCV) {
-            if (byte == C_UA) {
-              set_frame_state = C_RCV;
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          } else if (set_frame_state == C_RCV) {
-            if (byte == (buf[1] ^ buf[2])) {
-              set_frame_state = BCC_OK;
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          } else if (set_frame_state == BCC_OK) {
-            if (byte == FLAG) {
-              set_frame_state = SUCCESS;
-            
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          }
-          if (set_frame_state == SUCCESS) {
-            printf("UA frame received successfully\n");
-            STOP = TRUE;
-          }
-        }
-    }
-    else if (connectionParameters.role == LlRx) {
-        unsigned char buf[BUF_SIZE] = {0};
-
-        int nBytesBuf = 0;
-
-        while (STOP == FALSE) {
-          // Read one byte from serial port.
-          // NOTE: You must check how many bytes were actually read by reading the
-          // return value. In this example, we assume that the byte is always read,
-          // which may not be true.
-          unsigned char byte;
-          int bytes = readByteSerialPort(&byte);
-        
-          if (bytes < 0) {
-            perror("readByteSerialPort");
-            exit(-1);
-          } else if (bytes == 0) {
-            continue; // No byte received, try again
-          }
-      
-          buf[nBytesBuf] = byte;
-          nBytesBuf += bytes;
-      
-          if (set_frame_state == START) {
-            if (byte == FLAG) {
-              set_frame_state = FLAG_RCV;
-            }
-          } else if (set_frame_state == FLAG_RCV) {
-            if (byte == A_SENDER) {
-              set_frame_state = A_RCV;
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          } else if (set_frame_state == A_RCV) {
-            if (byte == C_SET) {
-              set_frame_state = C_RCV;
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          } else if (set_frame_state == C_RCV) {
-            if (byte == (buf[1] ^ buf[2])) {
-              set_frame_state = BCC_OK;
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          } else if (set_frame_state == BCC_OK) {
-            if (byte == FLAG) {
-              set_frame_state = SUCCESS;
-            
-            } else {
-              set_frame_state = START;
-              STOP = TRUE;
-            }
-          }
-          if (set_frame_state == SUCCESS) {
-            printf("SET frame received successfully\n");
-            unsigned char ua_frame[5] = {FLAG, A_RECEIVER, C_UA, A_RECEIVER ^ C_UA,
-                                         FLAG};
-            
-            bytes = writeBytesSerialPort(ua_frame, 5);
-            printf("UA frame sent\n");
-            
-            // Wait until all bytes have been written to the serial port
-            sleep(1);
-            STOP = TRUE;
-          }
-        }
-    
-        printf("Total bytes received: %d\n", nBytesBuf);
-
+        STOP = TRUE;
+      }
     }
 
+    printf("Total bytes received: %d\n", nBytesBuf);
+  }
 
-    return 0;
+  return 0;
 }
 
 ////////////////////////////////////////////////
 // LLWRITE
 ////////////////////////////////////////////////
-int llwrite(const unsigned char *buf, int bufSize)
-{
-    // TODO: Implement this function
+int llwrite(const unsigned char *buf, int bufSize) {
+  if (!ll_opened || ll_config.role != LlTx) {
+    return -1;
+  }
+  // TODO: Implement this function
+  // TODO: Dont send only data, put it in a format frame
+  // TODO: Byte stuffing?????
 
-    // TODO: Byte stuffing?????
+  int attempts = 0;
+  int maxAttempts = ll_config.nRetransmissions + 1;
+  int bytesWritten = 0;
 
-    return 0;
+  while (attempts < maxAttempts) {
+    bytesWritten = transmit_frame(buf, bufSize);
+    printf("%d\n", bytesWritten);
+    if (bytesWritten < 0) {
+      fprintf(stderr, "Failed to send data chunk\n");
+      return -1;
+    }
+    if (bytesWritten == bufSize) {
+      return bytesWritten; // success
+    }
+    attempts++;
+  }
+  return -1;
+
+  // NOTE: PSEUDOCODE
+  // while (attempts < max_attempts) {
+  //   send_frame(); // full stuffed I-frame
+
+  //   start_timer(saved_ll.timeout);
+
+  //   wait_for_ack_or_rej(); // block until RR/REJ or timeout
+
+  //   if (got_valid_RR_with_expected_Nr()) {
+  //     toggle_sequence_number();
+  //     return bufSize; // success
+  //   }
+  //   // else: retransmit (same seq num)
+  //   attempts++;
+  // }
+
+  // return -1; // all retries failed
 }
 
 ////////////////////////////////////////////////
 // LLREAD
 ////////////////////////////////////////////////
-int llread(unsigned char *packet)
-{
-    // TODO: Implement this function
+int llread(unsigned char *packet) {
+  // TODO: Implement this function
 
-    return 0;
+  return 0;
 }
 
 ////////////////////////////////////////////////
 // LLCLOSE
 ////////////////////////////////////////////////
-int llclose()
-{
-    if (closeSerialPort() < 0) {
-        perror("closeSerialPort");
-        exit(-1);
-    }
+int llclose() {
+  if (closeSerialPort() < 0) {
+    perror("closeSerialPort");
+    return -1;
+  }
 
-    printf("Serial port closed\n");
+  printf("Serial port closed\n");
 
-    return 0;
+  return 0;
 }
